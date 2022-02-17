c07af6666e54017964a95f26ac922537b65f4361
tarcisioalvesleop <tarcisioalvesleop@hotmail.com>
2022-02-17 00:36:26 -0300

    Definindo os moviemntos da cobrinha

--- script.js
+++ script.js
@@ -19,7 +19,23 @@ function criarCobrinha(){
     }
 }
 
+//captura os cliques no teclado e chama a função update
+document.addEventListener('keydown', update); 
+
+function update (event){//direciona a cobrinha
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    //retornando na outra parte da tela
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0; 
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
