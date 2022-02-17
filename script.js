80ca44228deb18ddcf343e328419fa0937608d66
tarcisioalvesleop <tarcisioalvesleop@hotmail.com>
2022-02-16 23:32:51 -0300

    criando movimento da cobrinha

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32; // tamanho de cada quadrado
 let snake = [];
-snake[0] = {
+snake[0] = {//tamanho da cobra
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";//direcção
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -14,9 +15,32 @@ function criarBG(){
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";//cor na cobrinha
-        context.fillRect(snake[i].x, snake[i].y, box, box); //tamanho d cobra e o tamanho do box
+        context.fillRect(snake[i].x, snake[i].y, box, box); //tamanho da cobra e o tamanho do box
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    //posicionamento da cobrinha
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    //coordenadas de deslocamento
+    if( direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+    
+    //retirando último elemento do array
+    snake.pop();
+
+    let newHead = {//cabeça da cobrinha 1 casa a frente
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);//a cada 100ms é atualizada a cobrinha
